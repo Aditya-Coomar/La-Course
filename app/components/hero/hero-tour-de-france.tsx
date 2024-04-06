ccf701543c0cea1a0f5082ac41a1f4c742feb8a1
Aditya-Coomar <adi.coomar04@gmail.com>
2024-04-07 05:07:56 +0530

    feat: add adjectives to header

--- app/components/hero/hero-tour-de-france.tsx
+++ app/components/hero/hero-tour-de-france.tsx
@@ -1,4 +1,5 @@
-import bicycle_clip_art from "~/assets/images/bicyler-clip-art.png"
+import bicycle_clip_art from "~/assets/images/bicyler-clip-art.png";
+import {ReactTyped} from "react-typed";
 
 export default function Hero({}) {
     return (
@@ -14,7 +15,21 @@ export default function Hero({}) {
             </div>
           </div>
           <div className="text-white italic font-extrabold text-[3rem] md:text-[5rem] md:pl-10 md:pt-[100px] vertical-center uppercase text-center md:text-left drop-shadow-lg">
-            Testing Endurance!
+          {"Testing "}
+          <ReactTyped
+            strings={[
+              "Endurance",
+              "Strength",
+              "Tactical Acumen",
+              "Innovation",
+            ]}
+            typeSpeed={70}
+            backSpeed={60}
+            backDelay={750}
+            showCursor={false}
+            loop
+          />
+          {"!"}
             <div className="date">29 June to 21 July</div>
             <div className="mt-10 md:mt-0 md:pt-3">
               <a href="#description_tour_de_france" type="button" className="align-middle py-3 px-5 md:py-4 md:px-6 bg-white text-xl md:text-2xl rounded-md animate-bounce"><span className="bg-gradient-to-r from-amber-300 to-neutral-900 bg-clip-text text-transparent">Know More</span></a>
